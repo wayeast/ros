e8fd3fa1fb9602ad6d1bfda9474de70e9d65533f
Huston <bokinsky@email.sc.edu>
2014-03-23 02:05:25 -0400

    Dressed up project3_node.cpp some

--- src/bokinsky_liles_wu_project3/src/project3_node.cpp
+++ src/bokinsky_liles_wu_project3/src/project3_node.cpp
@@ -1,33 +1,36 @@
+// std lib includes
 #include <stdlib.h>
 #include <stdio.h>
-//#include <iostream.h>
+
+// ROS includes
 #include <ros/ros.h>
 #include <std_msgs/Int32.h>
 
 int main(int argc, char **argv)
 {
+    // Basic ROS environment setup
+    ros::init(argc, argv, "publish_wifi_strength");
+    ros::NodeHandle nh;
+    ros::Publisher wifi_reporter = nh.advertise<std_msgs::Int32>(
+            "wifi_ss", 100);
+    std_msgs::Int32 msg;
+    ros::Rate rate(2);
+
     FILE *in;
     char buff[256];
-   ros::init(argc, argv, "publish_wifi_strength");
-   ros::NodeHandle nh;
+    while(ros::ok())
+    {
+        // Fetch wireless signal strength from system
+        if (!(in = popen("iwconfig wlan0|grep Quality|cut -d'=' -f2|cut -d'/' -f1", "r")))
+            return 1;
+        std::fgets(buff, sizeof(buff), in);
+        pclose(in);
 
-   ros::Publisher wifi_reporter = nh.advertise<std_msgs::Int32>(
-          "wifi_ss", 100);
-   std_msgs::Int32 msg;
-   ros::Rate rate(2);
+        // Publish wifi signal strength to /wifi_ss topic
+        msg.data = std::atoi(buff);
+        ROS_INFO_STREAM("Sending value " << msg.data << " to wifi_ss");
+        wifi_reporter.publish(msg);
 
-   while(ros::ok())
-   {
-       if (!(in = popen("iwconfig wlan0|grep Quality|cut -d'=' -f2|cut -d'/' -f1", "r")))
-       {
-           return 1;
-       }
-      
-       std::fgets(buff, sizeof(buff), in);
-       msg.data = std::atoi(buff);
-      ROS_INFO_STREAM("Sending value " << msg.data << " to wifi_ss");
-      wifi_reporter.publish(msg);
-      pclose(in);
-      rate.sleep();
-   }
+        rate.sleep();
+    }
 }
