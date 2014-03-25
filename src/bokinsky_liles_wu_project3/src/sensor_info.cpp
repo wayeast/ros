0e74b3ee933025495330201e2bed28a2bebc51e4
Huston <bokinsky@email.sc.edu>
2014-03-25 18:56:23 -0400

    touchups before switching to new workstation.  rviz crashing when adding robotmodel

--- src/bokinsky_liles_wu_project3/src/sensor_info.cpp
+++ src/bokinsky_liles_wu_project3/src/sensor_info.cpp
@@ -1,25 +1,35 @@
+#include <stdint.h>
+#include <iomanip>
+
 #include <ros/ros.h>
 #include <create_node/TurtlebotSensorState.h>
 #include <diagnostic_msgs/DiagnosticArray.h>
-#include <iomanip>
 
-void getInfo(const create_node::TurtlebotSensorState& msg){
-    if(msg.bumps_wheeldrops==15)
-        ROS_INFO_STREAM("Wheel drop caster.");
-    if(msg.bumps_wheeldrops==20)
-        ROS_INFO_STREAM("Wheel drop left.");
-    if(msg.bumps_wheeldrops==24)
+
+
+void getSensorInfo(const create_node::TurtlebotSensorState& msg)
+{
+    uint8_t mask = 1;
+    if (mask & msg.bumps_wheeldrops == 1)
+        ROS_INFO_STREAM("Bump right.");
+    mask << 1;
+    if (mask & msg.bumps_wheeldrops == 2)
+        ROS_INFO_STREAM("Bump left.");
+    mask << 1;
+    if (mask & msg.bumps_wheeldrops == 4)
         ROS_INFO_STREAM("Wheel drop right.");
-    if(msg.bumps_wheeldrops==28)
-        ROS_INFO_STREAM("Robot lifted.");
-    if(msg.bumps_wheeldrops==31)
-        ROS_INFO_STREAM("Robot bump sensor pressed");
+    mask << 1;
+    if (mask & msg.bumps_wheeldrops == 8)
+        ROS_INFO_STREAM("Wheel drop left.");
+    mask << 1;
+    if (mask & msg.bumps_wheeldrops == 16)
+        ROS_INFO_STREAM("Wheel drop caster.");
 }
 
 void getMsg(const diagnostic_msgs::DiagnosticArray& msg){
 
     for(int i =0; i < msg.status.size(); i++){
-        if(msg.status[i].level==1)	
+        if(msg.status[i].level==1)
             ROS_INFO_STREAM("WARNING from " <<
                     msg.status[i].name << ": " <<
                     msg.status[i].message);
@@ -39,7 +49,7 @@ int main(int argc, char **argv){
     ros::NodeHandle nh;
 
     ros::Subscriber sub = nh.subscribe("/turtlebot_node/sensor_state",
-            1000, &getInfo);
+            1000, &getSensorInfo);
     ros::Subscriber sub2 = nh.subscribe("/diagnostics",
             1000, &getMsg);
 
